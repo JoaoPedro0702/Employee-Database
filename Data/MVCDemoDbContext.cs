1572266352495bcb819c9705cd70668e3a27fe0d
JoaoPedro0702 <joaoptcantunes07@gmail.com>
2023-06-27 18:09:16 -0300

    first commit

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMVC.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions option) : base(option) 
+        { 
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
